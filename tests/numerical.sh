e899e231402601a6bc7ef9b672ed869c21547511
Andrei Kramer <andreikr@kth.se>
2022-02-13 00:51:11 +0100

    tests can now include comparisons to finite differences

---
+++ tests/numerical.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# here we read an expression from stdin and evaluate it using finite
+# differences (f(x+h)-f(x-h))/(2*h)
+
+# name of the variable
+var=${1:-x}
+val=${2:-0}
+h=${3:-0.001}
+
+while read rpn; do
+	#echo "${rpn}"
+	x_plus_h=`dc -e "${val} ${h} + p"`
+	x_minus_h=`dc -e "${val} ${h} - p"`
+	#echo "${x_plus_h}, ${x_minus_h}"
+	fx_plus_h=`echo "${rpn}" | sed -e "s/${var}/${x_plus_h}/g"`
+	fx_minus_h=`echo "${rpn}" | sed -e "s/${var}/${x_minus_h}/g"`
+	#echo "${fx_plus_h}, ${fx_minus_h}"
+	dc -e "${fx_plus_h} ${fx_minus_h} - 2 $h * / p"	
+done
--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+echo "linked lists"
+echo "============"
 [ -f ./tests/ll_test ] && ./tests/ll_test
+
+echo "binaries"
+echo "========"
+echo -n "test: derivative of 'x y *' w.r.t y\t\t\t"
 [ -f ./bin/derivative ] && echo "x y *" | bin/derivative y | sed -e 's/0 y [*] x 1 [*] +/success/'
+echo -n "test: simplify 'x 0 *'\t\t\t\t"
+[ -f ./bin/simplify ] && echo "x 0 *"  | bin/simplify | sed -e 's/0/success/'
 
