b09cb4066c560a35561ea2686c2136c0fcc74f18
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2022-10-14 14:19:50 +0200

    sh/ode.sh seems to work well, now it must be checkled for correctness.

--- sh/ode.sh
+++ sh/ode.sh
@@ -5,7 +5,7 @@ N=${2:-6}
 TMP=${3:-/dev/shm/ode_gen}
 
 # make sure the temp folder exists
-[ -d "${TMP}" ] || mkdir "${TMP}"
+[ -d "${TMP}" ] || (mkdir "${TMP}" || TMP='.')
 
 OPTTIONS="-type f"
 
@@ -17,16 +17,16 @@ EXP=`find . $OPTIONS -regex ".*[Ee]xpressions?\([Ff]ormulae?\)?\.t[xs][vt]$" -pr
 ODE=`find . $OPTIONS -iregex ".*ode\.t[xs][vt]$" -print -quit`
 
 # print some help if something is not right
-if [ -f "$VAR" -a -f "$PAR" -a -f "$ODE" ]; then
-	echo "[$0] Using these files:" 1>&2
-	echo "CON «$CON»" 1>&2
-	echo "PAR «$PAR»" 1>&2
-	echo "VAR «$VAR»" 1>&2
-	echo "EXP «$EXP»" 1>&2
-	echo "FUN «$FUN»" 1>&2
-	echo "ODE «$ODE»" 1>&2
-else
 (
+if [ -f "$VAR" -a -f "$PAR" -a -f "$ODE" ]; then
+	echo "[$0] Using these files:"
+	echo "CON «$CON»"
+	echo "PAR «$PAR»"
+	echo "VAR «$VAR»"
+	echo "EXP «$EXP»"
+	echo "FUN «$FUN»"
+	echo "ODE «$ODE»"
+else
 	echo "Usage: $0 [ModelName] [N] [TMP]"
 	echo;
 	echo "This assumes that `pwd` contains the four mandatory files:"
@@ -49,7 +49,6 @@ else
 	echo "Some temporary files will be created in ${TMP}, this location can be changed by setting the third command line argument."
 	echo "All derivatives will be simplified N times. (simplication means: «x+0=x» or «x*1=x», and similar things)"
 	echo "The default model name is 'DemoModel'."
-) 1>&2
 fi
 
 if [ -z `which derivative` ]; then
@@ -58,19 +57,24 @@ if [ -z `which derivative` ]; then
 	echo "	then you can use an alias (named 'derivative'):"
 	echo "		alias derivative='./bin/derivative'"
 fi
+) 1>&2
 
 NV=`wc -l < "$VAR"`
 NP=`wc -l < "$PAR"`
-if [ -f "$EXP" ] ; then
-	NE=`wc -l < "$EXP"`
-else
-	NE=0
-fi
+[ -f "$EXP" ] && NE=`wc -l < "$EXP"` || NE=0
+[ -f "$FUN" ] && NF=`wc -l < "$FUN"` || NF=0
 
-# make a copy of ODE.txt, but with all Fluxes substituted
+(
+echo "$NV state variables, $NP parameters, $NE expressions, $NF functions"
+echo "y-jacobian df[i]/dy[j] has size $((NV*NV)) ($NV×$NV)"
+echo "p-jacobian df[i]/dp[j] has size $((NV*NP)) ($NV×$NP)"
+) 1>&2
+
+# make a copy of ODE.txt, but with all expressions substituted
 EXODE="${TMP}/explicit_ode.txt"
-sed -r -e 's/(exp|sin|cos|tan)/@\1/g' -e 's/^-/0 - /g' "$ODE" > "$EXODE"
-
+## step 1, remove unary plusses and minuses
+sed -r -e 's/(exp|sin|cos|tan)/@\1/g' -e 's/^([+-][a-zA-Z_])/0 \1/g' -e 's|\([ ]*([+-][ ]*[a-zA-Z_][a-zA-Z_0-9]*[ ]*)\)|(0\1)|g' "$ODE" > "$EXODE"
+## step 2 substitute expression names for their values (formulae)
 if [ -f "$EXP" ]; then
 	for j in `seq $NE -1 1`; do
 		ExpressionName=`awk -F '	' --assign j=$j -e 'NR==j {print $1}' "$EXP"`
@@ -81,16 +85,12 @@ fi
 
 for j in `seq 1 $NV`; do
 	sv=`sed -n -e "${j}p" "$VAR"`
-	for i in `seq 1 $NV`; do
-		to_rpn < "$EXODE" | derivative $sv | simplify $N | to_infix > "${TMP}/Jac_Column_${j}.txt"
-	done
+	to_rpn < "$EXODE" | derivative $sv | simplify $N | to_infix > "${TMP}/Jac_Column_${j}.txt"
 done
 
 for j in `seq 1 $NP`; do
 	par=`sed -n -e "${j}p" "$PAR"`
-	for i in `seq 1 $NV`; do
-		to_rpn < "$EXODE" | derivative $par | simplify $N | to_infix > "${TMP}/Jacp_Column_${j}.txt"
-	done
+	to_rpn < "$EXODE" | derivative $par | simplify $N | to_infix > "${TMP}/Jacp_Column_${j}.txt"
 done
 
 
@@ -152,7 +152,7 @@ EOF
 awk '{print "\tdouble " $1 "=p_[" NR-1 "];"}' "$PAR"
 awk '{print "\tdouble " $1 "=y_[" NR-1 "];"}' "$VAR"
 [ -f "$EXP" ] && awk -F '	' '{print "\tdouble " $1 "=" $2 ";"}' "$EXP"
-for j in `seq 1 $NV`; do
+for j in `seq 1 $NP`; do
 	echo "/* column $j (df/dp_$((j-1))) */"
 	awk -v n=$NP -v j=$j '{print "\tjacp_[" (NR-1)*n + (j-1) "] = " $0 ";"}' $TMP/Jacp_Column_${j}.txt
 done
