f8b647be597368b7598df6f3c507c8ebe0aa174f
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2022-10-23 21:53:45 +0200

    sh/ode.sh can now generate R code and C code

--- sh/ode.sh
+++ sh/ode.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
-MODEL=${1:-"DemoModel"}
+MODEL="DemoModel"
+N=10
+PL="C"
+
+while [ $# -gt 0 ]; do
+ case $1 in
+ -C) PL="C"; shift;;
+ -R) PL="R"; shift;;
+ [0-9]*) N=$2; shift 2;;
+ -n) N=$2; shift 2;;
+ -t) TMP="$2"; shift 2;;
+ -c) CLEAN="yes"; shift;;
+ *) MODEL="$1"; shift;;
+ esac
+done
+
 BM=`basename "${MODEL}"`
 #echo $BM
-N=${2:-6}
 # check whether /dev/shm exists
 [ -d /dev/shm ] && TMPD="/dev/shm/ode_gen" || TMPD="/tmp/ode_gen"
 TMP=${3:-$TMPD} # override from command line (optionally)
 
 # make sure the temp folder exists
-[ -d "$TMP" ] || (mkdir "$TMP" || TMP='.')
+[ -d "$TMP" ] || mkdir "$TMP" || TMP='.'
 
 {
 if [ -f "$MODEL" -a "${BM#*.}" = "zip"  ]; then
@@ -164,9 +178,8 @@ for j in `seq 1 $NP`; do
 	to_rpn < "$EXODE" | derivative $par | simplify $N | to_infix > "${TMP}/Jacp_Column_${j}.txt"
 done
 
-
+write_in_C () {
 # write a gsl ode model file:
-
 cat<<EOF
 #include <stdlib.h>
 #include <math.h>
@@ -271,10 +284,102 @@ awk '{print "\tdouble " $1 "=p_[" NR-1 "];"}' "$PAR"
 printf "\t/* the initial value of y may depend on the parameters. */\n"
 awk '{print "\ty_[" NR-1 "] = " $2 ";"}' "$VAR"
 printf "\treturn GSL_SUCCESS;\n}\n"
+}
+
+write_in_R () {
+# write a gsl ode model file:
+cat<<EOF
+require("deSolve")
+
+# ode vector field: y'=f(t,y;p)
+${MODEL}_vf <- function(t, state, parameters)
+{
+EOF
+[ -f "$CON" ] && awk '{print "\t" $1 "<-" $2 }' "$CON"
+awk '{print "\t" $1 "<-parameters[" NR "]"}' "$PAR"
+awk '{print "\t" $1 "<-state[" NR "]"}' "$VAR"
+[ -f "$EXP" ] && awk -F '	' '{print "\t" $1 "<-" $2}' "$EXP"
+printf "\tf_<-vector(len=%i)" $NV
+awk -F '	' '{print "\tf_[" NR "] <- " $0 }' "$ODE"
+echo "\treturn(f_);"
+echo "}"
+
+cat<<EOF
+# ode Jacobian df(t,y;p)/dy 
+${MODEL}_jac<-function(t, state, parameters)
+{
+EOF
+[ -f "$CON" ] && awk '{print "\t" $1 "<-" $2}' "$CON"
+awk '{print "\t" $1 "<-parameters[" NR-1 "]"}' "$PAR"
+awk '{print "\t" $1 "<-state[" NR-1 "]"}' "$VAR"
+[ -f "$EXP" ] && awk -F '	' '{print "\t" $1 "<-" $2 }' "$EXP"
+printf "\tjac_<-matrix(%i,%i)" $NV $NV
+for j in `seq 1 $NV`; do
+	echo "# column $j (df/dy_$((j-1)))"
+	awk -v n=$NV -v j=$j '{print "\tjac_[" NR "," j "] <- " $0 }' $TMP/Jac_Column_${j}.txt
+done
+echo "\treturn(jac_);"
+echo "}"
+
+cat<<EOF
+# ode parameter Jacobian df(t,y;p)/dp
+${MODEL}_jacp<-function(t, state, parameters)
+{
+EOF
+[ -f "$CON" ] && awk '{print "\t" $1 "<-" $2}' "$CON"
+awk '{print "\t" $1 "<-parameters[" NR "]"}' "$PAR"
+awk '{print "\t" $1 "<-state[" NR "]"}' "$VAR"
+[ -f "$EXP" ] && awk -F '	' '{print "\t" $1 "<-" $2 }' "$EXP"
+printf "\tjacp_<-matrix(%i,%i)" $NV $NP
+for j in `seq 1 $NP`; do
+	echo "# column $j (df/dp_$((j)))"
+	awk -v n=$NP -v j=$j '{print "\tjacp_[" NR "," j "] <- " $0 }' $TMP/Jacp_Column_${j}.txt
+done
+echo "\treturn(jacp_)"
+echo "}"
+
+
+cat<<EOF
+# ode Functions F(t,y;p)
+${MODEL}_func<-function(t, state, parameters)
+{
+EOF
+
+[ -f "$CON" ] && awk '{print "\t" $1 "<-" $2 }' "$CON"
+awk '{print "\t" $1 "<-parameters[" NR "]"}' "$PAR"
+awk '{print "\t" $1 "<-state[" NR "]"}' "$VAR"
+[ -f "$EXP" ] && awk -F '	' '{print "\t" $1 "<-" $2 }' "$EXP"
+[ -f "$FUN" ] && awk '{print "\tfunc_[" NR "] <- " $0 }' "$FUN"
+echo "\treturn(func_);"
+echo "}"
+
+
+cat<<EOF
+# ode default parameters; can depend on constants, and time  of initialization
+${MODEL}_default<-function(t)
+{
+EOF
+[ -f "$CON" ] && awk '{print "\t" $1 "<-" $2 }' "$CON"
+awk '{print "\tparameters[" NR "] <- " $2 }' "$PAR"
+printf "\treturn(parameters);\n}\n"
+
+cat<<EOF
+# ode initial values
+${MODEL}_init<-function(t, parameters)
+{
+EOF
+[ -f "$CON" ] && awk '{print "\t" $1 "<-" $2 }' "$CON"
+awk '{print "\t" $1 "<-parameters[" NR "]"}' "$PAR"
+printf "\t# the initial value may depend on the parameters. \n"
+printf "\tstate<-vector(%i)" $NV
+awk '{print "\tstate[" NR "] <- " $2 }' "$VAR"
+printf "\treturn(state)\n}\n"
+}
+
+write_in_$PL
 
 ## cleaning procedure
-#if [ -d $TMP ]; then
-#	rm $TMP/*.rm
-#	rm $TMP/d*_d*.txt
-#	rm $TMP/Jac*_Column_*.txt
-#fi
+if [ "$CLEAN" -a -d $TMP ]; then
+	rm $TMP/d*_d*.txt
+	rm $TMP/Jac*_Column_*.txt
+fi
