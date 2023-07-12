f8e5db5fa4fbd47db13fca675bf37137a7d17da1
Andrei Kramer <andreikr@kth.se>
2023-07-12 17:44:25 +0200

    yacas backend works for the DemoModel on GNU/Linux

--- sh/yacas.sh
+++ sh/yacas.sh
@@ -1,6 +1,7 @@
 # yacas is weird about printing things on screen, but passing linesone by one seems to work.
 yacas_derivative () {
-	arg=${1/_/UNDERSCORE}
+	# yacas does not allow _ in symbol names, nor other special characters, like ~
+	arg=`echo $1 | sed 's/_/UNDERSCORE/g'`
 	while read f ; do
 		echo "Simplify(D($arg) $f)" | sed -E -f "${dir:-.}/yacas.sed" | yacas -c -f | sed -E -f "${dir:-.}/yacas-to-${PL:-C}.sed"
 	done
