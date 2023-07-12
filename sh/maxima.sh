ebbefa4d447bbbf35b0a15e764c20a73d4c8c5c7
Andrei Kramer <andreikr@kth.se>
2023-07-12 18:14:42 +0200

    fixed some issues with maxima backend related to «;» and «$»

--- sh/maxima.sh
+++ sh/maxima.sh
@@ -1,7 +1,7 @@
 # this defines the maxima backend
 maxima_derivative () {
 	if [ $# -gt 0 ]; then
-		awk -v X="$1" "BEGIN {print \"display2d:false\$\"}; {print \"diff(\" \$0 \",\"X\",1);\"};" | maxima --very-quiet | sed -E -e '1d' -f ${dir:-.}/maxima-to-${PL:-C}.sed
+		awk -v X="$1" "BEGIN {print \"display2d:false\$\n linel:1000\$\n\"}; {print \"diff(\" \$0 \",\"X\",1);\n\"};" | sed -E -f "${dir:-.}/maxima.sed" | maxima --very-quiet | sed -E -e '1d' -f ${dir:-.}/maxima-to-${PL:-C}.sed
 	else
 		echo "«$@» [missing mandatory argument (variable name)]"
 	fi
