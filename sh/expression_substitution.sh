cee6a5ee46889148074dec5b7157a84fbba1c8d0
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2025-01-13 20:37:10 +0100

    expression substitution bug fixed

--- sh/expression_substitution.sh
+++ sh/expression_substitution.sh
@@ -2,12 +2,12 @@
 
 ## substitute EXPRESSION_FILE MATH_FILE
 substitute () {
-    NE=$(( `wc -l < "$1"` ))
-    if [ $NE -gt 0 ]; then
-	awk -F '\t' '{print "s|\\b" $1 "\\b|(" $2 ")|g;"}' "$1" > "${TMP:-}/substitution_table.sed"
-	perl -p "${TMP:-.}/substitution_table.sed" "$2"
-    else
-	cat "$2"
-    fi
+	NE=$(( `wc -l < "$1"` ))
+	if [ $NE -gt 0 ]; then
+		tac "$1" | awk -F '\t' '{print "s|\\b" $1 "\\b|(" $2 ")|g;"}' > "${TMP:-}/substitution_table.sed"
+		perl -p "${TMP:-.}/substitution_table.sed" "$2"
+	else
+		cat "$2"
+	fi
 }
 
